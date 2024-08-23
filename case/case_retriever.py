1282ecc314e214f1942e9a6d56d1a664ef3511fd
clement.l <maple3312936@outlook.com>
2024-08-23 14:54:12 +0800

    refactor: update prompt

--- case/case_retriever.py
+++ case/case_retriever.py
@@ -1,13 +1,13 @@
 from langchain_chroma import Chroma
 
 from case.case_loader import get_case_doc_loader
-from case_code import CASE_TRANSFORMED_PATH
+from case_code import CASE_CONVERTED_PATH
 from utils.model_selector import get_embedding, get_chat_model
 
 
 # Load the case documents
 model_name = get_chat_model().name
-loader = get_case_doc_loader(CASE_TRANSFORMED_PATH.format(model=model_name))
+loader = get_case_doc_loader(CASE_CONVERTED_PATH.format(model=model_name))
 docs = loader.load()
 # Create the embedding model
 embedding_model = get_embedding()
--- graph/tools/batch.py
+++ graph/tools/batch.py
@@ -2,11 +2,11 @@ from langchain_core.tools import tool
 from langchain.pydantic_v1 import BaseModel
 
 
-async def transform_to_batch(query: str):
+async def convert_to_batch(query: str):
     """
-    If the query requires multiple steps to accomplish, it can be transformed into a batch.
+    If the query requires multiple steps to accomplish, it can be converted into a batch.
     Args:
-        query (str): The query to transform.
+        query (str): The query to convert.
     Returns:
         batch (str): The batch.
     """
