18af82cfdac1f25b5371613171c9fb4f14f0506d
clement.l <maple3312936@outlook.com>
2024-08-28 16:44:13 +0800

    feat: remove total_steps from case

--- models/case.py
+++ models/case.py
@@ -1,36 +1,6 @@
 from langchain_core.pydantic_v1 import BaseModel, Field
 
 
-# class Tx(BaseModel):
-#     """
-#     Represents a blockchain transaction with relevant details including the recipient,
-#     value, and function call information.
-
-#     Attributes:
-#         description (str): A detailed description of the transaction and its purpose.
-#         to (str): The blockchain address of the recipient for this transaction.
-#         value (str): The amount of the native token being transferred in this transaction.
-#         function_name (str): The name of the function being invoked in this transaction.
-#         input_args (list[str]): A list of input arguments passed to the function in the transaction, if any.
-#     """
-
-#     description: str = Field(
-#         description="A detailed description of the transaction and its purpose."
-#     )
-#     to: str = Field(
-#         description="The blockchain address of the recipient for this transaction."
-#     )
-#     value: str = Field(
-#         description="The amount of the native token being transferred in this transaction."
-#     )
-#     function_name: str = Field(
-#         description="The name of the function being invoked in this transaction."
-#     )
-#     input_args: list[str] = Field(
-#         description="A list of input arguments passed to the function in the transaction, if any."
-#     )
-
-
 class Transaction(BaseModel):
     """
     A transaction is an action taking place on a blockchain network.
@@ -52,9 +22,9 @@ class BatchCase(BaseModel):
     description: str = Field(
         description="A comprehensive description providing context, background, and the purpose of the batch case."
     )
-    total_steps: int = Field(
-        description="The total number of transactions included in this case."
-    )
+    # total_steps: int = Field(
+    #     description="The total number of transactions included in this case."
+    # )
     steps: list[Transaction] = Field(
         description="A sequential list of transactions (Tx), where each step represents to a single transaction."
     )
